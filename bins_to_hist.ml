5b0e35fc3b79e069fe5a7e82938ba8a80a0d0ada
Will M. Farr <w-farr@northwestern.edu>
2010-05-20 06:04:57 -0500

    New way of plotting histogram output.

--- bins_to_hist.ml
+++ bins_to_hist.ml
@@ -1,44 +1,27 @@
 (** Turns a list of bin boundaries into points on the PDF.  The points
     are evenly spaced between the maximum and minimum mass. *)
 
-let npts = ref 10
+open Printf
+
+let nskip = ref 1
 
 let options = 
-  [("-npts", Arg.Set_int npts,
-    Printf.sprintf "number of points to sample histogram at (default %d)" !npts)]
+  [("-nskip", Arg.Set_int nskip,
+    sprintf "plot one histogram for every nskip lines on stdin (default %d)" !nskip)]
 
-let find_interval_start bdys (m : float) = 
-  let n = Array.length bdys in 
-  if m < bdys.(0) || m >= bdys.(n-1) then raise (Invalid_argument "find_interval_start: value not inside interval");  
-  let rec find_interval_loop lowi highi = 
-    if highi - lowi = 1 then 
-      lowi
-    else
-      let midi = (highi + lowi) / 2 in 
-        if bdys.(midi) <= m then 
-          find_interval_loop midi highi
-        else
-          find_interval_loop lowi midi in 
-    find_interval_loop 0 (n-1)
-
-let eval_histogram bdys m = 
-  let pbin = 1.0 /. (float_of_int (Array.length bdys - 1)) in 
-  let ilow = find_interval_start bdys m in 
-  let width = bdys.(ilow+1) -. bdys.(ilow) in 
-    pbin /. width
-
-let write_one_hist bdys = 
-  let n = Array.length bdys in 
-  let dm = (bdys.(n-1) -. bdys.(0)) /. (float_of_int !npts) in 
-    for i = 0 to !npts - 1 do 
-      let m = bdys.(0) +. ((float_of_int i) +. 0.5)*.dm in 
-        Printf.printf "%g " (eval_histogram bdys m)
-    done;
-    Printf.printf "\n"
+let write_one_histogram bins = 
+  let nbins = Array.length bins - 1 in 
+  let area = 1.0 /. (float_of_int nbins) in 
+    for i = 0 to Array.length bins - 2 do 
+      let x = 0.5*.(bins.(i) +. bins.(i+1)) and 
+          y = area /. (bins.(i+1) -. bins.(i)) in 
+        printf "%g %g\n" x y
+    done
 
 let _ = 
-  Arg.parse options (fun _ -> ()) "bins_to_hist.{native,byte} OPTIONS ...";
+  Arg.parse options (fun _ -> ()) "bins_to_hist OPTIONS ... < input_file > output_file";
   let samples = Read_write.read (fun pt -> pt) stdin in 
-    Array.iter 
-      (fun {Mcmc.value = v} -> write_one_hist v)
-      samples
+    for i = 0 to Array.length samples - 1 do 
+      if i mod !nskip = 0 then 
+        let {Mcmc.value = bins} = samples.(i) in write_one_histogram bins
+    done
