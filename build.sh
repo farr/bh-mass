21d22abd70e4a5bdc68b9422251b9a67ac3916b7
Will M. Farr <w-farr@northwestern.edu>
2010-06-17 17:19:01 -0500

    Updates to build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,27 @@
 #!/bin/bash
 
-ocamlbuild all.otarget
-cp _build/*.native .
+set -e
+
+OCAMLBUILD=ocamlbuild
+
+ocb ()
+{
+    $OCAMLBUILD $FLAGS $*
+}
+
+rule() {
+    case $1 in
+        clean) ocb -clean;;
+        all) ocb all.otarget;;
+        *) echo "Unknown action ($1)"
+    esac;
+}
+
+if [ $# -eq 0 ]; then 
+    rule all
+else
+    while [ $# -gt 0 ]; do
+        rule $1
+        shift
+    done
+fi
