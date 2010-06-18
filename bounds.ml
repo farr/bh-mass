33d5d24f7c67ca2e11cf92358c276c1915697d7b
Will M. Farr <w-farr@northwestern.edu>
2010-06-18 16:19:11 -0500

    Code for low and high bounds.

--- bounds.ml
+++ bounds.ml
@@ -10,7 +10,6 @@ type dist =
 
 let which_dist = ref None
 let filename = ref ""
-let gap_bdy = ref 5.0
 let outfile = ref ""
 
 let options = 
@@ -24,82 +23,81 @@ let options =
     "use an exponential mcmc output in the given file");
    ("-two-gaussian", Arg.String (fun s -> which_dist := Two_gaussian; filename := s),
     "use the two-gaussian mcmc output in the given file");
-   ("-mgap", Arg.Set_float gap_bdy,
-    sprintf "maximum mass of the gap region (default %g)" !gap_bdy);
    ("-o", Arg.Set_string outfile, "output filename")]
 
-let hist_gap_overlap bins = 
-  let n = Array.length bins in 
-  let bvol = 1.0 /. (float_of_int (n-1)) in
-  let rec loop i ol = 
-    if i >= n - 1 then 
-      1.0 
-    else
-      let low = bins.(i) and 
-          high = bins.(i+1) in 
-      if high <= !gap_bdy then 
-        loop (i+1) (ol +. bvol)
-      else if low > !gap_bdy then 
-        ol
-      else
-        let frac = (!gap_bdy -. low)/.(high-.low) in 
-          ol +. frac*.bvol in
-    loop 0 0.0
-        
-let gauss_gap_overlap = function 
+let hist_bounds (bins : float array) = 
+  [|bins.(0); bins.(Array.length bins - 1)|]
+
+let gaussian_bounds = function 
   | [|mu; sigma|] -> 
-    0.5*.(1.0 +. (Gsl_sf.erf ((!gap_bdy -. mu) /. ((sqrt 2.0)*.sigma))))
-  | _ -> raise (Invalid_argument "gauss_gap_overlap: bad state")
+    [|mu -. 2.3263478740408411009*.sigma;
+      mu +. 2.3263478740408411009*.sigma|]
+  | _ -> raise (Invalid_argument "gaussian_bounds: bad state")
 
-let power_law_gap_overlap = function 
-  | [|mmin; mmax; alpha|] -> 
-    if !gap_bdy < mmin then 
-      0.0
-    else if !gap_bdy > mmax then 
-      1.0
-    else
-      let ap1 = alpha +. 1.0 in 
-      let mminap1 = mmin**ap1 in
-        ((!gap_bdy)**ap1 -. mminap1) /. (mmax**ap1 -. mminap1)
-  | _ -> raise (Failure "power_law_gap_overlap: bad state")
-
-let exponential_gap_overlap = function 
+let exponential_bounds = function 
   | [|mmin; msc|] -> 
-    if !gap_bdy >= mmin then 
-      1.0 -. (exp ((mmin -. !gap_bdy) /. msc))
+    [|mmin; mmin +. msc*.(log 100.0)|]
+  | _ -> raise (Invalid_argument "exponential_bounds: bad state")
+
+let power_law_bounds = function 
+  | [|mmin; mmax; alpha|] -> 
+    [|mmin; mmax|]
+  | _ -> raise (Invalid_argument "power_law_bounds: bad state")
+
+let solve f x0 = 
+  let dx = 
+    let rec dx_loop dx = 
+      let fxlow = f (x0 -. dx) and 
+          fxhigh = f (x0 +. dx) in 
+        if fxlow *. fxhigh <= 0.0 then 
+          dx
+        else
+          dx_loop (2.0*.dx) in 
+      dx_loop 1.0 in 
+  let rec loop x0 x1 fx0 fx1 = 
+    if abs_float (x0 -. x1) < 1e-8 then 
+      (fx0*.x1 -. fx1*.x0) /. (fx0 -. fx1)
     else
-      0.0
-  | _ -> raise (Invalid_argument "exponential_gap_overlap: bad state")
+      let xmid = 0.5*.(x0 +. x1) in 
+      let fxmid = f xmid in 
+        if fx0 *. fxmid <= 0.0 then 
+          loop x0 xmid fx0 fxmid
+        else
+          loop xmid x1 fxmid fx1 in 
+  let x0 = x0 -. dx and 
+      x1 = x0 +. dx in 
+    loop x0 x1 (f x0) (f x1)
 
-let two_gaussian_gap_overlap = function 
+let two_gaussian_bounds = function 
   | [|mu1; mu2; sigma1; sigma2; a|] -> 
-    a*.(gauss_gap_overlap [|mu1; sigma1|]) +. (1.0-.a)*.(gauss_gap_overlap [|mu2; sigma2|])
-  | _ -> raise (Invalid_argument "two_gaussian_gap_overlap: bad state")
-
-let compare_float (x : float) y = Pervasives.compare x y
+    let f frac x = 
+      frac +. 
+        a*.(Gsl_sf.erf ((x -. mu1)/.(1.4142135623730950488*.sigma1))) +. 
+        (1.0 -. a)*.(Gsl_sf.erf ((x -. mu2) /. (1.4142135623730950488*.sigma2))) in 
+    let fmin x = f (49.0/.100.0) x and 
+        fmax x = f (-49.0/.100.0) x in 
+      [| solve fmin 0.0; solve fmax 0.0|]
+  | _ -> raise (Invalid_argument "two_gaussian_bounds: bad state")
 
 let _ = 
-  Arg.parse options (fun _ -> ()) "gaptest.{byte,native} OPTIONS ...";
+  Arg.parse options (fun _ -> ()) "bounds.{byte,native} OPTIONS ...";
+  let get_bounds = 
+    match !which_dist with 
+      | Gaussian -> gaussian_bounds
+      | Exponential -> exponential_bounds
+      | Power_law -> power_law_bounds
+      | Histogram -> hist_bounds
+      | Two_gaussian -> two_gaussian_bounds
+      | _ -> raise (Failure "You must specify a distribution to read from.") in 
   let inp = open_in !filename in 
-  let mcmcs = Read_write.read (fun x -> x) inp in 
+  let samples = Read_write.read (fun x -> x) inp in 
     close_in inp;
-    let overlaps = 
-      match !which_dist with 
-        | Histogram -> 
-          Array.map (fun {Mcmc.value = x} -> hist_gap_overlap x) mcmcs
-        | Gaussian -> 
-          Array.map (fun {Mcmc.value = x} -> gauss_gap_overlap x) mcmcs
-        | Power_law -> 
-          Array.map (fun {Mcmc.value = x} -> power_law_gap_overlap x) mcmcs 
-        | Exponential -> 
-          Array.map (fun {Mcmc.value = x} -> exponential_gap_overlap x) mcmcs
-        | Two_gaussian -> 
-          Array.map (fun {Mcmc.value = x} -> two_gaussian_gap_overlap x) mcmcs
-        | _ -> eprintf "You must specify some distribution via a command line argument.\n";
-          exit 1 in
     let out = open_out !outfile in 
-    let n = Array.length overlaps in 
-      Array.iter (fun x -> fprintf out "%g\n" x) overlaps;
-      close_out out;
-      Array.fast_sort compare_float overlaps;
-      eprintf "90%% of overlaps less than %g\n" (overlaps.((n*9)/10))
+      Array.iter 
+        (fun {Mcmc.value = samp} -> 
+          match get_bounds samp with 
+            | [|low; high|] -> 
+              fprintf out "%g %g\n" low high
+            | _ -> raise (Failure "bad bounds"))
+        samples;
+      close_out out
