6d8cc46f49ec52cb48f4b9141546194746b8f3d3
Will M. Farr <w-farr@northwestern.edu>
2010-06-22 14:13:47 -0500

    Modified skip_filter.ml to reduce number of MCMC samples.

--- skip_filter.ml
+++ skip_filter.ml
@@ -1,10 +1,19 @@
+let infile = ref ""
+
+let options = 
+  [("-i", Arg.Set_string infile, "input file")]
+
 let _ = 
-  let inp = open_in "power-law.mcmc" in 
+  Arg.parse options (fun _ -> ()) "skip_filter.{byte,native} OPTIONS ...";
+  let infile = !infile in 
+  let inp = open_in infile in 
   let samples = Read_write.read (fun x -> x) inp in 
     close_in inp;
-    let out = open_out "power-law.mcmc.tmp" in 
+    let outfile = infile ^ ".tmp" in
+    let out = open_out outfile in 
       for i = 0 to Array.length samples - 1 do 
-        if i mod 3 <> 0 then 
+        if i mod 5 = 0 then 
           Read_write.write_sample (fun x -> x) out samples.(i)
       done;
-      close_out out
+      close_out out;
+      Unix.rename outfile infile
