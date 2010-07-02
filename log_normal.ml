d9d92dc41ebc11148fa0605c0e94dc6832306fce
Will M. Farr <w-farr@northwestern.edu>
2010-07-02 12:11:09 -0700

    Added log_normal.ml to distributions.

---
+++ log_normal.ml
@@ -0,0 +1,54 @@
+open Printf
+open Logn_base
+
+(* These are provided by the base module. *)
+(* let mmin = ref 0.0 *)
+(* let mmax = ref 40.0 *)
+let nmsamp = ref 1000
+let nmcmc = ref 30000
+let nskip = ref 100
+let nbin = ref 10000
+let outfile = ref "log-normal.mcmc"
+let overwrite = ref false
+
+let options = 
+  [("-mmin", Arg.Set_float mmin, 
+    sprintf "minimum mass (default %g)" !mmin);
+   ("-mmax", Arg.Set_float mmax,
+    sprintf "maximum mass (default %g)" !mmax);
+   ("-nmsamp", Arg.Set_int nmsamp,
+    sprintf "number of samples to take from mass distributions (default %d)" !nmsamp);
+   ("-nmcmc", Arg.Set_int nmcmc,
+    sprintf "number of MCMC samples to output (default %d)" !nmcmc);
+   ("-nskip", Arg.Set_int nskip,
+    sprintf "number of MCMC samples to skip between outputs (default %d)" !nskip);
+   ("-nbin", Arg.Set_int nbin,
+    sprintf "number of burn-in samples (default %d)" !nbin);
+   ("-seed", Arg.Int (fun s -> Random.init s), "initialize RNG with given seed");
+   ("-o", Arg.Set_string outfile, 
+    sprintf "output file for the mcmc (default %s)" !outfile);
+   ("-overwrite", Arg.Set overwrite,
+    "overwrite the pre-existing MCMC samples in output file")]
+
+let _ = 
+  Random.self_init ();
+  Arg.parse options (fun _ -> ()) "log_normal.{byte,native} OPTIONS ...";
+  let msamples = Masses.generate_samples !nmsamp in
+  let log_likelihood x = log_likelihood msamples x in
+  let s0 = [|log 9.97; 0.01|] in 
+  let current = ref {Mcmc.value = s0;
+                     like_prior = {Mcmc.log_prior = log_prior s0;
+                                   log_likelihood = log_likelihood s0}} in
+  let next = Mcmc.make_mcmc_sampler log_likelihood log_prior jump_proposal log_jump_prob in 
+    for i = 1 to !nbin do
+      current := next !current
+    done;
+    let flags = (if !overwrite then Open_trunc else Open_append) :: [Open_wronly; Open_creat; Open_text] in
+    let out = open_out_gen flags 0o644 !outfile in 
+      for i = 1 to !nmcmc do
+        for j = 1 to !nskip do
+          current := next !current
+        done;
+        Read_write.write_sample (fun x -> x) out !current
+      done;
+      close_out out
---
+++ logn_base.ml
@@ -0,0 +1,41 @@
+let mmin = ref 0.01
+let mmax = ref 40.0
+
+let log_prior = function 
+  | [|mu; sigma|] -> 
+    let lmmax = log !mmax and 
+        lmmin = log !mmin in
+    if lmmin <= mu && mu <= lmmax && sigma >= 0.0 && 
+      sigma <= 0.5*.(lmmax -. mu) then 
+      let dlm = lmmax -. lmmin in 
+        1.3862943611198906188 -. 2.0*.(log dlm)
+    else
+      neg_infinity
+  | _ -> raise (Invalid_argument "log_prior: bad state")
+
+let log_likelihood msamples = function 
+  | [|mu; sigma|] -> 
+    List.fold_left
+      (fun ll msamp -> 
+        let n = Array.length msamp in 
+        let sum = ref 0.0 in 
+          for i = 0 to n - 1 do 
+            sum := !sum +. (exp (Stats.log_lognormal mu sigma msamp.(i)))
+          done;
+          ll +. (log (!sum /. (float_of_int n))))
+      0.0
+      msamples
+  | _ -> raise (Invalid_argument "log_likelihood: bad state")
+
+let jump_proposal = function 
+  | [|mu; sigma|] -> 
+    let dm = 0.2 in (* times 1.1, 0.9 *)
+    let ds = 0.2 in
+      [|mu +. dm*.(Random.float 1.0 -. 0.5);
+        sigma +. ds*.(Random.float 1.0 -. 0.5)|]
+  | _ -> raise (Invalid_argument "jump_proposal: bad state")
+
+let log_jump_prob _ _ = 0.0
+        
+let low_bounds () = [|log !mmin; 0.0|]
+let high_bounds () = [|log !mmax; 0.5*.(log !mmax)|]
