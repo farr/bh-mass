a9ddfbd9e071fc8cbdd5b325bc105e6fd20eb94e
Will M. Farr <w-farr@northwestern.edu>
2010-07-12 13:05:25 -0500

    Refactored histogram.ml

--- histogram.ml
+++ histogram.ml
@@ -1,8 +1,9 @@
 open Printf
 open Dist_base
+open Histogram_base
 
-let nbinmax = ref 5
-let fixedbin = ref false
+(* let nbinmax = ref 5 *) (* Comes from base. *)
+(* let fixedbin = ref false *) 
 let numbin = ref 1
 let outfile = ref "histogram.mcmc"
 let overwrite = ref false
@@ -19,53 +20,6 @@ let options =
                    sprintf "output file name (default %s)" !outfile);
                   ("-overwrite", Arg.Set overwrite,
                    "overwrite the output file instead of appending to it")])
-
-let compare_float (x : float) y = Pervasives.compare x y
-
-let log_likelihood msamples = 
-  let msamples = 
-    List.map 
-      (fun msamp -> 
-        let msamp = Array.copy msamp in 
-          Array.fast_sort compare_float msamp;
-          msamp)
-      msamples in 
-    fun bins -> 
-      List.fold_left
-        (fun ll msamp -> 
-          let nm = Array.length msamp and 
-              nb = Array.length bins in
-          let rec overlap_loop im ib ol = 
-            if im >= nm then 
-              ol /. (float_of_int nm)
-            else if ib >= nb - 1 then 
-              ol /. (float_of_int nm)
-            else
-              let m = msamp.(im) and 
-                  low = bins.(ib) and 
-                  high = bins.(ib+1) in
-                if m < low then 
-                  overlap_loop (im+1) ib ol
-                else if m >= high then 
-                  overlap_loop im (ib+1) ol
-                else
-                  overlap_loop (im+1) ib (ol +. 1.0 /. ((float_of_int (nb-1))*.(high-.low))) in 
-          let overlap = overlap_loop 0 0 0.0 in 
-            ll +. (log overlap))
-        0.0
-        msamples
-
-let log_factorial n = 
-  let sum = ref 0.0 in 
-    for i = 2 to n do 
-      sum := !sum +. (log (float_of_int i))
-    done;
-    !sum
-
-let log_prior bins = 
-  let log_bin_factor = if !fixedbin then 0.0 else ~-.(log (float_of_int !nbinmax)) in
-  let n = Array.length bins in 
-    (log_factorial n) +. log_bin_factor -. (float_of_int n)*.(log (!mmax -. !mmin))
 
 let decrease_bins bins = 
   let n = Array.length bins in 
--- reversible_jump.ml
+++ reversible_jump.ml
@@ -1,6 +1,7 @@
 open Rj_base
 module Ec = Expc_base
 module G = Gaussian_base
+module H = Histogram_base
 
 let mmin = 0.0 
 let mmax = 40.0
@@ -27,55 +28,6 @@ module Interp = Interpolate_pdf.Make(struct
 
   let point (c : float array) = c
 end)
-
-module H = struct 
-  let compare_float (x : float) y = Pervasives.compare x y
-
-  let log_likelihood msamples = 
-    let msamples = 
-      List.map 
-        (fun msamp -> 
-          let msamp = Array.copy msamp in 
-            Array.fast_sort compare_float msamp;
-            msamp)
-        msamples in 
-      fun (bins : float array) -> 
-        List.fold_left
-          (fun (ll : float) (msamp : float array) -> 
-            let nm = Array.length msamp and 
-                nb = Array.length bins in
-            let rec overlap_loop im ib ol = 
-              if im >= nm then 
-                ol /. (float_of_int nm)
-              else if ib >= nb - 1 then 
-                ol /. (float_of_int nm)
-              else
-                let m = msamp.(im) and 
-                    low = bins.(ib) and 
-                    high = bins.(ib+1) in
-                  if m < low then 
-                    overlap_loop (im+1) ib ol
-                  else if m >= high then 
-                    overlap_loop im (ib+1) ol
-                  else
-                    overlap_loop (im+1) ib (ol +. 1.0 /. ((float_of_int (nb-1))*.(high-.low))) in 
-            let overlap = overlap_loop 0 0 0.0 in 
-              ll +. (log overlap))
-          0.0
-          msamples
-
-  let log_factorial n = 
-    let sum = ref 0.0 in 
-      for i = 2 to n do 
-        sum := !sum +. (log (float_of_int i))
-      done;
-      !sum
-
-  let log_prior bins = 
-    let log_bin_factor = 0.0 in
-    let n = Array.length bins in 
-      (log_factorial n) +. log_bin_factor -. (float_of_int n)*.(log (mmax -. mmin))
-end
 
 module Pl = struct 
   let log_likelihood msamples = function 
