f27c36261c05f7d3d815a74e745d02c5e2a475b7
Will M. Farr <w-farr@northwestern.edu>
2010-07-16 12:11:54 -0400

    Better rounding in the percentile of combine_rj.

--- combine_rj.ml
+++ combine_rj.ml
@@ -24,13 +24,24 @@ let read_file file =
 let compare_dim (dim : int) (a : float array) (b : float array) = 
   Pervasives.compare a.(dim) b.(dim)
 
-let med_5_95 dim probs = 
+let round f = 
+  if f >= 0.0 then 
+    int_of_float (f +. 0.5)
+  else
+    int_of_float (f -. 0.5)
+
+let med_5_95 ?(lowf = 0.05) ?(highf = 0.95) dim probs = 
   let nrj = Array.length probs in 
   let comp a b = compare_dim dim a b in
-  let med = (Stats.find_nthf ~copy:false comp (nrj / 2) probs).(dim) and 
-      low = (Stats.find_nthf ~copy:false comp (nrj / 20) probs).(dim) and 
-      high = (Stats.find_nthf ~copy:false comp ((19*nrj)/20) probs).(dim) in 
-    (med, low, high)
+  let medi = round (0.5*.(float_of_int nrj)) and 
+      lowi = round (lowf*.(float_of_int nrj)) and 
+      highi = round (highf*.(float_of_int nrj)) in
+    if highi >= nrj then 
+      raise (Failure (Printf.sprintf "combine_rj: you need more samples to explore the %g-th percentile" highf));
+    let med = (Stats.find_nthf ~copy:false comp medi probs).(dim) and 
+        low = (Stats.find_nthf ~copy:false comp lowi probs).(dim) and 
+        high = (Stats.find_nthf ~copy:false comp highi probs).(dim) in 
+      (med, low, high)
 
 let _ = 
   Arg.parse [] (fun s -> filenames := s :: !filenames) "combine_rj.{byte,native} FILE ...";
