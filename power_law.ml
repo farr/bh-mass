6c0d1be5c88acc7cc8158aed371b9fb53d725d82
Will M. Farr <w-farr@northwestern.edu>
2010-07-18 20:42:11 -0400

    New, simpler jump proposal for power_law.ml.

--- power_law.ml
+++ power_law.ml
@@ -18,69 +18,34 @@ let options =
                   ("-overwrite", Arg.Set overwrite,
                    "overwrite the output file")])
 
-let shift = function 
+let jump_proposal = function 
   | [|min_mass; max_mass; alpha|] -> 
-    let left_bound = max !mmin (min_mass -. 0.5) and 
-        right_bound = min !mmax (max_mass +. 0.5) and
-        d = max_mass -. min_mass in 
-    let new_min = Stats.draw_uniform left_bound (right_bound -. d) in 
-      [|new_min; new_min +. d; alpha|]
-  | _ -> raise (Invalid_argument "shift: bad state")
-
-let log_shift_jp x y = 
-  match x,y with 
-    | [|min_mass1; max_mass1; alpha1|], [|min_mass2; max_mass2; alpha2|] when alpha1 = alpha2 -> 
-      let left_bound = max !mmin (min_mass1 -. 0.5) and 
-          right_bound = min !mmax (max_mass1 +. 0.5) and 
-          d = max_mass1 -. min_mass1 in 
-        ~-.(log ((right_bound -. d) -. left_bound))
-    | _ -> neg_infinity
-
-let stretch = function 
-  | [|min_mass; max_mass; alpha|] -> 
-    let d = max_mass -. min_mass and 
-        c = 0.5 *. (max_mass +. min_mass) in 
-    let c_bounds_dist = min (!mmax -. c) (c -. !mmin) in
-    let min_d = max 0.0 (d -. 0.5) and 
-        max_d = min (2.0*.c_bounds_dist) (d +. 0.5) in
-    let new_d = Stats.draw_uniform min_d max_d in 
-      [| c -. 0.5*.new_d; c +. 0.5*.new_d; alpha |] 
-  | _ -> raise (Invalid_argument "stretch: bad state")
-
-let log_stretch_jp x y = 
-  match x,y with 
-    | [|min_mass1; max_mass1; alpha1|], [|min_mass2; max_mass2; alpha2|]
-      when abs_float (0.5*.(max_mass1 -. min_mass1) -. 0.5*.(max_mass2 -. min_mass2)) < 1e-8 
-        && alpha1 = alpha2 -> 
-      let d = max_mass1 -. min_mass1 and 
-          c = 0.5 *. (max_mass1 +. min_mass1) in 
-      let c_bounds_dist = min (!mmax -. c) (c -. !mmin) in
-      let min_d = max 0.0 (d -. 0.5) and 
-          max_d = min (2.0*.c_bounds_dist) (d +. 0.5) in
-        ~-.(log (max_d -. min_d))
-    | _ -> neg_infinity
-
-let tilt = function 
-  | [|min_mass; max_mass; alpha|] -> 
+    let mid_mass = 0.5*.(min_mass +. max_mass) in 
+    let min_min = max !mmin (min_mass -. 1.0) and 
+        min_max = min mid_mass (min_mass +. 1.0) and 
+        max_min = max mid_mass (max_mass -. 1.0) and 
+        max_max = min !mmax (max_mass +. 1.0) in 
     let amin = max !alphamin (alpha -. 0.1) and 
         amax = min !alphamax (alpha +. 0.1) in 
-      [|min_mass; max_mass; Stats.draw_uniform amin amax|]
-  | _ -> raise (Invalid_argument "tilt: bad state")
+      [|Stats.draw_uniform min_min min_max;
+        Stats.draw_uniform max_min max_max;
+        Stats.draw_uniform amin amax|]
+  | _ -> raise (Invalid_argument "jump_proposal: bad state")
 
-let log_tilt_jp x y = 
-  match x,y with 
-    | [|min_mass1; max_mass1; alpha1|], [|min_mass2; max_mass2; alpha2|] 
-      when min_mass1 = min_mass2 && max_mass1 = max_mass2 -> 
-      let amin = max !alphamin (alpha1 -. 0.1) and 
-          amax = min !alphamax (alpha1 +. 0.1) in 
-        ~-.(log (amax -. amin))
-    | _ -> neg_infinity
-      
-let (jump_proposal, log_jump_probability) = 
-  Mcmc.combine_jump_proposals
-    [(1.0, shift, log_shift_jp);
-     (1.0, stretch, log_stretch_jp);
-     (1.0, tilt, log_tilt_jp)]
+let log_jump_probability source _ = 
+  match source with 
+    | [|min_mass; max_mass; alpha|] -> 
+          let mid_mass = 0.5*.(min_mass +. max_mass) in 
+          let min_min = max !mmin (min_mass -. 1.0) and 
+              min_max = min mid_mass (min_mass +. 1.0) and 
+              max_min = max mid_mass (max_mass -. 1.0) and 
+              max_max = min !mmax (max_mass +. 1.0) in 
+          let amin = max !alphamin (alpha -. 0.1) and 
+              amax = min !alphamax (alpha +. 0.1) in 
+            ~-.((log (min_max -. min_min)) +. 
+                   (log (max_max -. max_min)) +. 
+                   (log (amax -. amin)))
+    | _ -> raise (Invalid_argument "log_jump_prob: bad source state")
 
 let _ = 
   Randomize.randomize ();
