6b3b309ca89abcd7881a206e9cf1a9a438eb7aab
Will M. Farr <w-farr@northwestern.edu>
2010-09-01 18:20:16 -0500

    Added choose_mcmc_arg.sh utility script.

---
+++ choose_mcmc_arg.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+case $1 in 
+    histogram*) echo "-histogram";;
+    gaussian*) echo "-gaussian";;
+    power-law*) echo "-power-law";;
+    two-gaussian*) echo "-two-gaussian";;
+    exp*) echo "-exponential";;
+    log-normal*) echo "-log-normal";;
+    reversible*) echo "-rj";;
+    *) exit 1;;
+esac
+
